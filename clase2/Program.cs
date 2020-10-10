5bf99c7f2f6636449230d4068ef18cd03c4b00fa
neolabar <dante.panella.contractor@gdevtools.com>
2020-10-09 21:16:18 -0300

    fin de la clase2

--- clase2/Program.cs
+++ clase2/Program.cs
@@ -4,9 +4,35 @@ namespace clase2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)  // nuesntro punto de inicio
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("inicio del programa");
+
+            saludarEspEng();
+
+            Console.WriteLine("fin del programa");
+
+        }
+
+        static void saludarEspEng()
+        {
+            saludarEsp("hola soy argentino");
+
+            saludarEng();
+        }
+
+        static void saludarEsp(string mensaje)
+        {
+
+            Console.WriteLine(mensaje.ToUpper());
+        }
+
+
+        static void saludarEng()
+        {
+
+            Console.WriteLine("Welcome to the summer house");
+
         }
     }
 }
